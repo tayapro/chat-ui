076c86e3b88571cea80bcfa43157fd594af09984
Taya Simakova <stipaxa@gmail.com>
2023-05-22 18:06:24 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card text-center border-0">
+        <!-- <div class="card-header border-0"></div> -->
+        <div class="card-body">blaa</div>
+        <div class="card-footer fixed-bottom border-0">
+            <InputMessage @new-message="hello" />
+        </div>
+    </div>
+</template>
+
+<script>
+import InputMessage from './components/InputMessage.vue'
+
+export default {
+    name: 'App',
+    components: { InputMessage },
+    methods: {
+        hello() {
+            console.log('function bla')
+        },
+    },
+}
+</script>
+
+<style>
+#app {
+    border: none;
+    /* font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px; */
+}
+</style>
---
+++ src/components/InputMessage.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="New message ..." />
+        <button
+            type="button"
+            class="btn btn-primary btn-md"
+            @click="$emit('new-message')"
+        >
+            Send
+        </button>
+    </div>
+    <!-- <div>
+        <form>
+            <div class="mb-3 w-50">
+                <input
+                    class="m-2 mt-4 border rounded-pill input-sm"
+                    id="typeText"
+                />
+                <button
+                    class="btn btn-primary btn-md"
+                    type="button"
+                    @click="$emit('new-message')"
+                >
+                    Send
+                </button>
+            </div>
+        </form>
+    </div> -->
+    <!-- <div>
+        <input class="m-2 mt-4 border rounded-pill input-sm" id="typeText" />
+        <button
+            class="m-2 btn btn-info rounded-pill btn-sm"
+            @click="$emit('new-message')"
+        >
+            Button
+        </button>
+    </div> -->
+</template>
+
+<script>
+export default {
+    name: 'InputMessage',
+    props: {
+        msg: String,
+    },
+}
+</script>
